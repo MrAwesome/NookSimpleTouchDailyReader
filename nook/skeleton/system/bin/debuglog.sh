df06860eb5aa8f805c3cc28d605cd7e1d4959364
Glenn Hope <glenn.alexander.hope@gmail.com>
2022-07-30 21:07:29 -0400

    Add multiple formats and nook fix

--- nook/skeleton/system/bin/debuglog.sh
+++ nook/skeleton/system/bin/debuglog.sh
@@ -1,6 +1,8 @@
 #!/system/xbin/ash
 
 set -euo pipefail
+
+FORMATS="pdf epub"
                 
 # NOTE: This file must be populated with your own static server IP address for this to work!
 ADDR=$(cat /system/bin/ADDR)
@@ -18,12 +20,19 @@ if [[ -f "$CHECKER_FILENAME" ]]; then
     fi
 fi
 
+
+if [[ ! -f $CHECKER_FILENAME ]]; then
+    echo "latest" > $CHECKER_FILENAME
+fi
 CACHED_CHECKED=$(cat $CHECKER_FILENAME)
 
 CHECKER_CONTENTS=$(wget -q "http://$ADDR/$CHECKER_FILENAME" -O - || echo "$CACHED_CHECKED")
+
 echo "$CHECKER_CONTENTS" > "$CHECKER_FILENAME"
 
-TARGET_FILENAME=$(cat $CHECKER_FILENAME).epub
-if [[ ! -f "$TARGET_FILENAME" ]]; then
-    wget -q "http://$ADDR/$TARGET_FILENAME"
-fi
+for format in FORMATS; do
+    TARGET_FILENAME=$(cat $CHECKER_FILENAME).${format}
+    if [[ ! -f "$TARGET_FILENAME" ]]; then
+        wget -q "http://$ADDR/$TARGET_FILENAME"
+    fi
+done
--- server/rubyreader/run.sh
+++ server/rubyreader/run.sh
@@ -13,7 +13,7 @@ mkdir -p "$BUILD_DIR"
 ruby main.rb
 
 for format in $FORMATS; do
-    ebook-convert "$BUILD_DIR"/index.html "$BUILD_DIR"/"$OUTF.${format}"
+    ebook-convert "$BUILD_DIR"/index.html "$BUILD_DIR"/"$OUTF.${format}" --output-profile=nook
 done
 
 #adb connect 192.168.1.97:5555
